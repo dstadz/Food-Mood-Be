b5208d70e6a3624f1483969a90388a5dae95a1b1
Daniel <danstad2012@gmail.com>
2020-03-28 19:35:40 -0700

    introduce styled components

---
+++ food-mood FE/src/landingpage.ts
@@ -0,0 +1,13 @@
+// my-theme.ts
+import { DefaultTheme } from '../styled'
+
+const myTheme: DefaultTheme = {
+  borderRadius: '5px',
+
+  colors: {
+    main: 'cyan',
+    secondary: 'magenta',
+  },
+}
+
+export { myTheme }
